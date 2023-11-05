773eeaba2bd286c9c6957dae4c2984ca45de70e6
VuDuc1610 <90380300+VuDuc1610@users.noreply.github.com>
2023-11-05 05:15:58 -0500

    Add find halls and doors

--- front-end/roc-map/src/App.js
+++ front-end/roc-map/src/App.js
@@ -1,39 +1,87 @@
 import hallsData from "./utils/halls.json";
-import "./App.css";
+import doorsData from "./utils/doors.json";
+import doorDescription from "./utils/doorDescription.json";
 import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 import "./mediaqueries.css";
 
 function App() {
   const [arrInfo, setArrInfo] = useState([{}]);
   const [showResult, setShowResult] = useState(false);
-  const [isSuggestionsVisible, setIsSuggestionsVisible] = useState(false);
-
+  const [isStartVisible, setIsStartVisible] = useState(false);
+  const [isEndVisible, setIsEndVisible] = useState(false);
   const [formData, setFormData] = React.useState({
     start: "",
     end: "",
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-    setIsSuggestionsVisible(!!value);
+  const [startValue, setStartValue] = useState("");
+  const [destinationValue, setDestinationValue] = useState("");
+  
+
+  // Inside your component function
+  const [filteredHallsFrom, setFilteredHallsFrom] = useState([]);
+  const [filteredHallsTo, setFilteredHallsTo] = useState([]);
+
+  const [selectedStartLocation, setSelectedStartLocation] = useState(null);
+  const [selectedEndLocation, setSelectedEndLocation] = useState(null);
+  const [availableDoors, setAvailableDoors] = useState([]);
+
+  const [startDoorId, setStartDoorId] = useState(null);
+
+
+  const handleStartChange = (e) => {
+    const { value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      start: value,
+    }));
+    setIsStartVisible(!!value);
+    setFilteredHallsFrom(
+      hallsData.filter((hall) =>
+        hall.name.toLowerCase().includes(value.toLowerCase())
+      )
+    );
   };
 
-  const handleAutoCompleteChange = (e) => {
-    const value = e.value; 
-    setFormData({
-      ...formData,
-      start: value, 
-    });
-    setIsSuggestionsVisible(false);
+  const handleEndChange = (e) => {
+    const { value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      end: value,
+    }));
+    setIsEndVisible(!!value);
+    setFilteredHallsTo(
+      hallsData.filter((hall) =>
+        hall.name.toLowerCase().includes(value.toLowerCase())
+      )
+    );
   };
 
-  const filteredHalls = hallsData.filter((hall) =>
-    hall.name.toLowerCase().includes(formData.start.toLowerCase())
-  );
+  const handleAutoCompleteChange = (hall, field) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [field]: hall.name,
+    }));
+
+    if (field === "start") {
+      setStartValue(hall.name);
+      setSelectedStartLocation(hall);
+    } else if (field === "end") {
+      setDestinationValue(hall.name);
+      setSelectedEndLocation(hall);
+    }
+
+    setIsStartVisible(false);
+    setIsEndVisible(false);
+
+    const matchingDoors = doorsData.find((door) => door.name === hall.name);
+    if (matchingDoors) {
+      setAvailableDoors(matchingDoors.doors);
+    } else {
+      setAvailableDoors([]); 
+    }
+  };
 
   const descriptionRef = useRef(null);
   useEffect(() => {
@@ -91,42 +139,111 @@ function App() {
         <p className="form-description"> Please input your desired route: </p>
         <div className="form-container">
           <div className="input-container">
-            <input
-              type="text"
-              placeholder="From"
-              onChange={handleChange}
-              name="start"
-              value={formData.start}
-              onFocus={() => setIsSuggestionsVisible(!!formData.start)}
-              onBlur={() => setIsSuggestionsVisible(false)}
-            />
-            {isSuggestionsVisible ? (
-              <div class="dropdown">
-              <ul className="autocomplete-list">
-                <div class="dropdown-content">
-                {filteredHalls.length > 0 ? (
-                  filteredHalls.map((hall, index) => (
-                    <li
-                      key={index}
-                      onClick={() => handleAutoCompleteChange(hall.name)}
-                    >
-                      {hall.name}
-                    </li>
-                  ))
-                ) : (
-                  <li>Nothing found</li>
-                )}
+            <div class="autocomplete-wrapper">
+              <input
+                type="text"
+                placeholder="From"
+                onChange={handleStartChange}
+                name="start"
+                value={formData.start}
+              />
+              {isStartVisible && formData.start ? (
+                <div class="dropdown active">
+                  <ul className="autocomplete-list">
+                    <div class="dropdown-content">
+                      {filteredHallsFrom.length > 0 ? (
+                        filteredHallsFrom.map((hall, index) => (
+                          <div
+                            key={index}
+                            onClick={() =>
+                              handleAutoCompleteChange(hall, "start")
+                            }
+                          >
+                            {hall.name}
+                          </div>
+                        ))
+                      ) : (
+                        <div>Nothing found</div>
+                      )}
+                    </div>
+                  </ul>
                 </div>
-              </ul>
-              </div>
-            ) : null}
-            <input
-              type="text"
-              placeholder="To"
-              onChange={handleChange}
-              name="end"
-              value={formData.end}
-            />
+              ) : null}
+            </div>
+
+            <div className="doors-dropdown">
+            {selectedStartLocation && availableDoors.length > 0 && (
+              <select onChange={(event) => {
+                console.log(event.target.value);
+                setStartDoorId(event.target.value);
+              }}>
+                <option value={null}>Select a door</option>
+                {availableDoors.map((door, index) => {
+                  const matchingDescription = doorDescription.find((desc) => desc.id === door);
+                  
+                  return (
+                  <option key={index} value={door}>
+                    {matchingDescription.doorDescription}
+                  </option>
+                )})}
+              </select>
+            )}
+          </div>
+
+            <div class="autocomplete-wrapper">
+              <input
+                type="text"
+                placeholder="To"
+                onChange={handleEndChange}
+                name="end"
+                value={formData.end}
+              />
+              {isEndVisible && formData.end ? (
+                <div class="dropdown active">
+                  <ul className="autocomplete-list">
+                    <div class="dropdown-content">
+                      {filteredHallsTo.length > 0 ? (
+                        filteredHallsTo.map((hall, index) => (
+                          <div
+                            key={index}
+                            onClick={() =>
+                              handleAutoCompleteChange(hall, "end")
+                            }
+                          >
+                            {hall.name}
+                          </div>
+                        ))
+                      ) : (
+                        <div>Nothing found</div>
+                      )}
+                    </div>
+                  </ul>
+                </div>
+              ) : null}
+            </div>
+
+            <div className="doors-dropdown">
+            {selectedEndLocation && availableDoors.length > 0 && (
+              <select>
+              <option value="">Select a door</option>
+              {availableDoors.map((door, index) => {
+                const matchingDescription = doorDescription.find((desc) => desc.id === door);
+
+                return (
+                <option key={index} value={door}>
+                  {matchingDescription.doorDescription}
+                </option>
+              )})}
+            </select>
+            )}
+          </div>
+
+          </div>
+
+
+          <div className="clarify-info">
+            <div>Start: {startValue} - {startDoorId && doorDescription.find((desc) => desc.id === startDoorId).doorDescription} </div>
+            <div>Destination: {destinationValue}</div>
           </div>
           <div className="button-container">
             <button className="submit-button" onClick={showResultDiv}>
